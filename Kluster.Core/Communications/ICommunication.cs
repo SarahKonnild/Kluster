5147aeb36b60e6a84718f830a7890eaa38e43520
Sarah Knapp <S_Pradel@icloud.com>
2025-02-06 22:03:12 +0100

    Started working on the source generators and defining the ICommunication interface to enable the source generator strategy

---
+++ Kluster.Core/Communications/ICommunication.cs
@@ -0,0 +1,10 @@
+﻿namespace Kluster.Core.Communications;
+
+/// <summary>
+///     A marker interface for the interface definitions that are used to define the receiving points of an application
+///     for import in the using side.
+///     I.e. An IServer is defined which implements ICommunication, the IServer interface defines [UdpEndpoint] and
+///     [TcpEndpoint] which the Clients can use to reach the server. Each different type of Endpoint generates its own
+///     method which applies the correct transfer protocol.
+/// </summary>
+public interface ICommunication;
